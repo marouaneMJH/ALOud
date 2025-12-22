2b8d3bdf1112e6b355887c1e3115ae177829601a
marouaneMJH <marwanmoujahid008@gmail.com>
2025-12-22 16:16:01 +0100

    Update .gitignore and project dependencies; add content exclusions and improve formatting

--- Data/ALOudDbContext.cs
+++ Data/ALOudDbContext.cs
@@ -13,6 +13,7 @@ namespace ALOud.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
 
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
